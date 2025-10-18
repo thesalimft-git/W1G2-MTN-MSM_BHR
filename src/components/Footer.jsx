5b9f1f004cf144d541de241168b84c55472c0560
sali mft <thesalimft@gmail.com>
2025-10-18 20:47:18 +0330

    end of session 18

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,84 @@ function Footer() {
   return (
     <>
       <div className="d-grid col-sm-1 col-md-2">
-        <div className="d-grid col-sm-1 col-md-2 ">
-          <div style={{ backgroundColor: 'red' }}> address</div>
-          <div style={{ backgroundColor: 'blue' }}> menu</div>
+        <div className="d-grid col-sm-1 col-md-2">
+          {/* address */}
+          <div>
+            <h3>
+              Address Information
+            </h3>
+            <div>
+              <p>
+                <strong>
+                  Email:
+                </strong>
+                <span className="ps-2">
+                  petshop@gmail.com
+                </span>
+              </p>
+            </div>
+            <div>
+              <p>
+                <strong>
+                  Phone:
+                </strong>
+                <span className="ps-2">
+                  0912-345-6789
+                </span>
+              </p>
+            </div>
+            <div>
+              <p>
+                <strong>
+                  Address:
+                </strong>
+                <span className="ps-2">
+                  N0.7. sample ave.
+                </span>
+              </p>
+            </div>
+          </div>
+
+          {/* menu */}
+          <div>
+            <h3>
+              Pages
+            </h3>
+            <div className="footer-menu">
+              <ul>
+                <li>
+                  <a href="#" className="menu-items">Store</a>
+                </li>
+                <li>
+                  <a href="#" className="menu-items">Card</a>
+                </li>
+                <li>
+                  <a href="#" className="menu-items">Ticket</a>
+                </li>
+                <li>
+                  <a href="#" className="menu-items">About Us</a>
+                </li>
+                <li>
+                  <a href="#" className="menu-items">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div style={{ backgroundColor: 'violet' }}> form</div>
+
+        {/* contact form */}
+        <div>
+          <h3>
+            Contact Us
+          </h3>
+          <div>
+            <form action="" className="d-flex-col">
+              <input className="form-control" type="text" placeholder="Full Name"/>
+              <input className="form-control" type="text" placeholder="Email"/>
+              <textarea className="form-control" placeholder="message ..." rows="7"></textarea>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   )
